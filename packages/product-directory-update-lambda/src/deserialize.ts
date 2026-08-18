17ce41b27f37b24b223e307c2d38aadfb9566bc2
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-08-18 09:24:14 +0100

    comment out thrift import givign es module error for now

--- packages/product-directory-update-lambda/src/deserialize.ts
+++ packages/product-directory-update-lambda/src/deserialize.ts
@@ -1,30 +1,30 @@
-import type { Event } from '@guardian/content-api-models/crier/event/v1/event';
-import { EventSerde } from '@guardian/content-api-models/crier/event/v1/event';
-import type { ItemResponse } from '@guardian/content-api-models/v1/itemResponse';
-import { ItemResponseSerde } from '@guardian/content-api-models/v1/itemResponse';
-import type { TagsResponse } from '@guardian/content-api-models/v1/tagsResponse';
-import { TagsResponseSerde } from '@guardian/content-api-models/v1/tagsResponse';
-import type { TProtocol } from 'thrift';
-import { TCompactProtocol, TFramedTransport } from 'thrift';
+// import type { Event } from '@guardian/content-api-models/crier/event/v1/event';
+// import { EventSerde } from '@guardian/content-api-models/crier/event/v1/event';
+// import type { ItemResponse } from '@guardian/content-api-models/v1/itemResponse';
+// import { ItemResponseSerde } from '@guardian/content-api-models/v1/itemResponse';
+// import type { TagsResponse } from '@guardian/content-api-models/v1/tagsResponse';
+// import { TagsResponseSerde } from '@guardian/content-api-models/v1/tagsResponse';
+// import type { TProtocol } from 'thrift';
+// import { TCompactProtocol, TFramedTransport } from 'thrift';
 
-function feedToThrift(content: string | Buffer): TProtocol {
-	const buffer = Buffer.isBuffer(content)
-		? content
-		: Buffer.from(content, 'base64');
-	const transport = new TFramedTransport(buffer);
-	return new TCompactProtocol(transport);
-}
+// function feedToThrift(content: string | Buffer): TProtocol {
+// 	const buffer = Buffer.isBuffer(content)
+// 		? content
+// 		: Buffer.from(content, 'base64');
+// 	const transport = new TFramedTransport(buffer);
+// 	return new TCompactProtocol(transport);
+// }
 
-export function deserializeEvent(content: string | Buffer): Event {
-	return EventSerde.read(feedToThrift(content));
-}
+// export function deserializeEvent(content: string | Buffer): Event {
+// 	return EventSerde.read(feedToThrift(content));
+// }
 
-export function deserializeItemResponse(
-	content: string | Buffer,
-): ItemResponse {
-	return ItemResponseSerde.read(feedToThrift(content));
-}
+// export function deserializeItemResponse(
+// 	content: string | Buffer,
+// ): ItemResponse {
+// 	return ItemResponseSerde.read(feedToThrift(content));
+// }
 
-export function deserialzeTagsResponse(content: string | Buffer): TagsResponse {
-	return TagsResponseSerde.read(feedToThrift(content));
-}
+// export function deserialzeTagsResponse(content: string | Buffer): TagsResponse {
+// 	return TagsResponseSerde.read(feedToThrift(content));
+// }
