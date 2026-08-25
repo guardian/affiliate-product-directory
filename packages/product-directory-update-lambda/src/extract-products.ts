e8256d3f736d7cd6f495df3992dcec395bb5cbe2
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-08-25 17:12:11 +0100

    feat: extract product URLs from blocks

--- packages/product-directory-update-lambda/src/extract-products.ts
+++ packages/product-directory-update-lambda/src/extract-products.ts
@@ -1,11 +1,16 @@
 import type { Block } from '@guardian/content-api-models/v1/block';
+import type { BlockElement } from '@guardian/content-api-models/v1/blockElement';
 import type { Blocks } from '@guardian/content-api-models/v1/blocks';
 import type { Content } from '@guardian/content-api-models/v1/content';
 import { ContentType } from '@guardian/content-api-models/v1/contentType';
 import { ElementType } from '@guardian/content-api-models/v1/elementType';
 import { LinkType } from '@guardian/content-api-models/v1/linkType';
+import type { ProductCTA } from '@guardian/content-api-models/v1/productCTA';
+import type { DirectoryProduct } from './models';
 
-export function extractAllProductsFromArticle(content: Content) {
+export function extractAllProductsFromArticle(
+	content: Content,
+): DirectoryProduct[] {
 	if (content.type == ContentType.ARTICLE && content.blocks) {
 		const articleBlocks: Blocks = content.blocks;
 		// TODO: why did recipes-backend check the main block?
@@ -13,23 +18,54 @@ export function extractAllProductsFromArticle(content: Content) {
 		const bodyBlocks = articleBlocks.body as Block[];
 
 		const bodyBlockProductElements = bodyBlocks.flatMap((bodyBlock) =>
-			extractProductElements(content, bodyBlock),
+			extractProductElements(bodyBlock),
 		);
 
 		const bodyProductButtons = bodyBlocks.flatMap((bodyBlock) =>
-			extractProductButtons(content, bodyBlock),
+			extractProductButtons(bodyBlock),
 		);
-		//ToDo: get the nested product buttons from the product element main content
-		void bodyBlockProductElements;
-		void bodyProductButtons;
 
-		return [];
+		const nestedButtonProductUrls = bodyBlockProductElements
+			.flatMap((block) =>
+				extractNestedProductButtons(block.productTypeData?.content),
+			)
+			.map(getProductUrlFromButton);
+		const productCTAUrls = bodyBlockProductElements
+			.flatMap(getProductCTAsFromProductElement)
+			.map(getProductUrlFromProductCTA);
+		const buttonProductURLs = bodyProductButtons.map(getProductUrlFromButton);
+
+		const deduplicatedURLs = new Set(
+			productCTAUrls
+				.concat(buttonProductURLs)
+				.concat(nestedButtonProductUrls)
+				.filter((url) => url !== undefined),
+		);
+		return [...deduplicatedURLs].map((url) => ({
+			productMerchantUrl: url,
+		}));
 	} else {
 		return [];
 	}
 }
 
-export function extractProductElements(content: Content, block: Block) {
+function getProductUrlFromButton(button: BlockElement): string | undefined {
+	return button.linkTypeData?.url;
+}
+
+function getProductUrlFromProductCTA(
+	productCTA: ProductCTA,
+): string | undefined {
+	return productCTA.url;
+}
+
+function getProductCTAsFromProductElement(
+	productElement: BlockElement,
+): ProductCTA[] {
+	return productElement.productTypeData?.productCtas ?? [];
+}
+
+function extractProductElements(block: Block) {
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- to fix error when elements are undefined , example if main block does not have any elements.
 	if (!block?.elements) {
 		return [];
@@ -38,7 +74,7 @@ export function extractProductElements(content: Content, block: Block) {
 	}
 }
 
-export function extractProductButtons(content: Content, block: Block) {
+function extractProductButtons(block: Block): BlockElement[] {
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- to fix error when elements are undefined , example if main block does not have any elements.
 	if (!block?.elements) {
 		return [];
@@ -49,4 +85,17 @@ export function extractProductButtons(content: Content, block: Block) {
 				elem.linkTypeData?.linkType === LinkType.PRODUCT_BUTTON,
 		);
 	}
+}
+
+function extractNestedProductButtons(
+	blockElements: BlockElement[] | undefined,
+): BlockElement[] {
+	if (blockElements === undefined) {
+		return [];
+	}
+	return blockElements.filter(
+		(elem) =>
+			elem.type === ElementType.LINK &&
+			elem.linkTypeData?.linkType === LinkType.PRODUCT_BUTTON,
+	);
 }
