56ceca136dea4c6600090bb97263363af6e1d9f3
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-09-01 15:43:48 +0100

    api pagination

--- packages/scripts/backfill/create-backfill-events.ts
+++ packages/scripts/backfill/create-backfill-events.ts
@@ -9,6 +9,8 @@ import { writeFile } from 'fs/promises';
 interface CapiSearchResponse {
 	response: {
 		status: string;
+		currentPage: number;
+		pages: number;
 		results: Array<{ id: string }>;
 	};
 }
@@ -27,9 +29,16 @@ function getBaseCapiUrl(stack: string): string {
 		: 'https://content.code.dev-guardianapis.com';
 }
 
-async function getArticleIds(stack: string): Promise<string[]> {
-	const capiKey = getCapiKey();
-	const url = `${getBaseCapiUrl(stack)}/search?section=thefilter&api-key=${capiKey}`;
+function getArticleIdsFromResponse(searchResponse: CapiSearchResponse) {
+	return searchResponse.response.results.map((result) => result.id);
+}
+
+async function getArticleIdsForPage(
+	stack: string,
+	capiKey: string,
+	page: number,
+): Promise<CapiSearchResponse> {
+	const url = `${getBaseCapiUrl(stack)}/search?section=thefilter&page=${page}&api-key=${capiKey}`;
 
 	const response = await fetch(url);
 	if (!response.ok) {
@@ -39,14 +48,30 @@ async function getArticleIds(stack: string): Promise<string[]> {
 	}
 
 	const body = (await response.json()) as CapiSearchResponse;
-	return body.response.results.map((result) => result.id);
+	return body;
+}
+
+async function getAllArticleIds(stack: string): Promise<string[]> {
+	const capiKey = getCapiKey();
+	let articleIds: string[] = [];
+	const firstPage = await getArticleIdsForPage(stack, capiKey, 1);
+	articleIds = articleIds.concat(getArticleIdsFromResponse(firstPage));
+
+	let page = 2;
+	while (page <= firstPage.response.pages) {
+		const nextPage = await getArticleIdsForPage(stack, capiKey, page);
+		articleIds = articleIds.concat(getArticleIdsFromResponse(nextPage));
+		page++;
+	}
+
+	return articleIds;
 }
 
 async function main() {
 	let stack = process.argv[2];
 	stack ??= 'CODE';
 
-	const articleIds = await getArticleIds(stack);
+	const articleIds = await getAllArticleIds(stack);
 
 	const formattedEvents = [
 		{
