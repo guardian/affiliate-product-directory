8f57a1123bd59dcf909e5eeba743a8316e80d446
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-09-03 14:44:55 +0100

    use series not commissioning desk

--- packages/product-directory-update-lambda/src/tag-utils.ts
+++ packages/product-directory-update-lambda/src/tag-utils.ts
@@ -1,10 +1,9 @@
 import type { Tag } from '@guardian/content-api-models/v1/tag';
 
-const FILTER_US_TRACKING_TAG_ID =
-	'tracking/commissioningdesk/filter-us' as const;
-const FILTER_UK_TRACKING_TAG_ID = 'tracking/commissioningdesk/the-filter';
+const FILTER_US_SERIES_TAG_ID = 'thefilter-us/series/thefilter-us';
+const FILTER_UK_SERIES_TAG_ID = 'thefilter/series/the-filter';
 
-const FILTER_TAGs = [FILTER_UK_TRACKING_TAG_ID, FILTER_US_TRACKING_TAG_ID];
+const FILTER_TAGs = [FILTER_UK_SERIES_TAG_ID, FILTER_US_SERIES_TAG_ID];
 
 export const isFilterArticleByTags = (tags: Tag[]) =>
 	tags.some((tag) => FILTER_TAGs.includes(tag.id));
@@ -18,9 +17,9 @@ export const getRegionFromTags = (tags: Tag[]): string | undefined => {
 	}
 
 	switch (filterTag.id) {
-		case FILTER_UK_TRACKING_TAG_ID:
+		case FILTER_UK_SERIES_TAG_ID:
 			return 'UK';
-		case FILTER_US_TRACKING_TAG_ID:
+		case FILTER_US_SERIES_TAG_ID:
 			return 'US';
 		default:
 			// This means we have a non-Filter article
