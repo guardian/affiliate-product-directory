fe831a51488be1b3004d87ff66d728321e61abd8
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-09-04 10:51:36 +0100

    ignore conditionalcheckfailedexception

--- packages/product-directory-update-lambda/src/database-service.ts
+++ packages/product-directory-update-lambda/src/database-service.ts
@@ -1,4 +1,9 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import type { PutItemCommandInput } from '@aws-sdk/client-dynamodb';
+import {
+	ConditionalCheckFailedException,
+	DynamoDBClient,
+	PutItemCommand,
+} from '@aws-sdk/client-dynamodb';
 import { dynamoConfig } from './aws-config';
 import type { ExtractedDirectoryProduct } from './models';
 
@@ -10,32 +15,40 @@ export class DynamoService {
 		private readonly client = new DynamoDBClient(dynamoConfig),
 	) {}
 
+	private async saveToDb(putItemCommand: PutItemCommandInput): Promise<void> {
+		try {
+			await this.client.send(new PutItemCommand(putItemCommand));
+		} catch (err) {
+			if (err instanceof ConditionalCheckFailedException) {
+				// item already exists; treat as a successful no-op
+			} else {
+				throw err;
+			}
+		}
+	}
+
 	async saveProduct({
 		pricing,
 		article,
 	}: ExtractedDirectoryProduct): Promise<void> {
 		await Promise.all([
-			this.client.send(
-				new PutItemCommand({
-					TableName: this.pricingTableName,
-					Item: {
-						productMerchantUrl: { S: pricing.productMerchantUrl },
-						region: { S: pricing.region },
-					},
-					ConditionExpression: 'attribute_not_exists(productMerchantUrl)',
-				}),
-			),
-			this.client.send(
-				new PutItemCommand({
-					TableName: this.articleTableName,
-					Item: {
-						productMerchantUrl: { S: article.productMerchantUrl },
-						articleUrl: { S: article.articleUrl },
-						composerArticleId: { S: article.composerArticleId ?? '' },
-					},
-					ConditionExpression: 'attribute_not_exists(productMerchantUrl)',
-				}),
-			),
+			this.saveToDb({
+				TableName: this.pricingTableName,
+				Item: {
+					productMerchantUrl: { S: pricing.productMerchantUrl },
+					region: { S: pricing.region },
+				},
+				ConditionExpression: 'attribute_not_exists(productMerchantUrl)',
+			}),
+			this.saveToDb({
+				TableName: this.articleTableName,
+				Item: {
+					productMerchantUrl: { S: article.productMerchantUrl },
+					articleUrl: { S: article.articleUrl },
+					composerArticleId: { S: article.composerArticleId ?? '' },
+				},
+				ConditionExpression: 'attribute_not_exists(productMerchantUrl)',
+			}),
 		]);
 	}
 }
