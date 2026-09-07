95adba49bf3296cbca93d7efd0466766e4ad3c92
Solomon Owen-Williams <solomon.owen-williams@guardian.co.uk>
2026-09-07 17:41:09 +0100

    refac

--- packages/product-price-update-lambda/src/price-providers/amazon/AmazonPriceProvider.ts
+++ packages/product-price-update-lambda/src/price-providers/amazon/AmazonPriceProvider.ts
@@ -5,7 +5,7 @@ export class AmazonPriceProvider extends PriceProvider {
 	protected readonly name = 'amazon';
 
 	public refreshPrices(products: Product[]): Promise<Product[]> {
-		// ToDo: call the Amazon PA-API (per-marketplace, batched, via this.withRetry)
+		// ToDo: call the Amazon API and return update product items
 		console.log(`Fetching Amazon prices for ${products.length} products`);
 		return Promise.resolve(products);
 	}
