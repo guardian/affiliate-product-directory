786ca7aabe8c870555112006ed4c2ccdca374623
Solomon Owen-Williams <solomon.owen-williams@guardian.co.uk>
2026-09-10 16:40:22 +0100

    fix test

--- packages/cdk/lib/affiliate-product-directory.ts
+++ packages/cdk/lib/affiliate-product-directory.ts
@@ -38,7 +38,7 @@ export class AffiliateProductDirectory extends GuStack {
 				// Used for defining cron job execution
 				rules: [
 					{
-						schedule: Schedule.cron({ hour: '17', minute: '40' }),
+						schedule: Schedule.cron({ hour: '17', minute: '50' }),
 						description: 'Product price update lambda',
 						input: undefined,
 					},
